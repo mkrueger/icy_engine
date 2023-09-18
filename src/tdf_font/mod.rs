4f29a5aa2862d08bbc28d5e42a448406b1ebae44
Mike Krüger <mkrueger@posteo.de>
2023-09-18 16:38:37 +0200

    Fixed some issues in tdf font rendering.

--- src/tdf_font/mod.rs
+++ src/tdf_font/mod.rs
@@ -27,6 +27,7 @@ impl FontGlyph {
 
         let mut cur = pos;
         let mut char_offset = 0;
+        let mut leading_space = true;
         while char_offset < self.data.len() {
             let ch = self.data[char_offset];
             char_offset += 1;
@@ -34,31 +35,58 @@ impl FontGlyph {
             if ch == 13 {
                 cur.x = pos.x;
                 cur.y += 1;
+                leading_space = true;
             } else {
                 let attributed_char = match font_type {
-                    FontType::Outline => AttributedChar::new(
-                        unsafe {
-                            char::from_u32_unchecked(TheDrawFont::transform_outline(
-                                outline_style,
-                                ch,
-                            ) as u32)
-                        },
-                        color,
-                    ),
+                    FontType::Outline => {
+                        if ch == b'@' || ch == b' ' && leading_space {
+                            cur.x += 1;
+                            continue;
+                        }
+                        leading_space = false;
+                        if ch == b'O' {
+                            AttributedChar::new(' ', color)
+                        } else {
+                            AttributedChar::new(
+                                unsafe {
+                                    char::from_u32_unchecked(TheDrawFont::transform_outline(
+                                        outline_style,
+                                        ch,
+                                    )
+                                        as u32)
+                                },
+                                color,
+                            )
+                        }
+                    }
                     FontType::Block => {
-                        AttributedChar::new(unsafe { char::from_u32_unchecked(ch as u32) }, color)
+                        if ch == b' ' {
+                            cur.x += 1;
+                            continue;
+                        }
+                        if ch == 0xF7 {
+                            AttributedChar::new(' ', color)
+                        } else {
+                            AttributedChar::new(
+                                unsafe { char::from_u32_unchecked(ch as u32) },
+                                color,
+                            )
+                        }
                     }
                     FontType::Color => {
                         let ch = unsafe { char::from_u32_unchecked(ch as u32) };
                         let ch_attr =
                             TextAttribute::from_u8(self.data[char_offset], BufferType::LegacyIce); // tdf fonts don't support ice mode by default
                         char_offset += 1;
-                        AttributedChar::new(ch, ch_attr)
+                        let ch = AttributedChar::new(ch, ch_attr);
+                        if ch.is_transparent() {
+                            cur.x += 1;
+                            continue;
+                        }
+                        ch
                     }
                 };
-                if !attributed_char.is_transparent() {
-                    editor.set_char(cur, attributed_char).unwrap();
-                }
+                editor.set_char(cur, attributed_char).unwrap();
                 cur.x += 1;
             }
         }
@@ -337,7 +365,11 @@ impl TheDrawFont {
 
     pub fn transform_outline(outline: usize, ch: u8) -> u8 {
         if ch > 64 && ch - 64 <= 17 {
-            TheDrawFont::OUTLINE_CHAR_SET[outline][(ch - 65) as usize]
+            if outline >= TheDrawFont::OUTLINE_CHAR_SET.len() {
+                ch
+            } else {
+                TheDrawFont::OUTLINE_CHAR_SET[outline][(ch - 65) as usize]
+            }
         } else {
             b' '
         }
